c1d7d72f77978a6c46fef32cbdd03e91cfcce5d5
xin <tsyw233@gmail.com>
2026-04-21 04:19:31 +0000

    mem-ruby: Add per-router avg stall time stats (EX-014)
    Add avg_stall_cy stat to Switch and Throttle for Fig 7 heatmap.
    
    - Throttle: avg_stall_cy = total_stall_cy / total_msg_count; accessors
      getStallCy() / getMsgCount()
    - Switch: aggregate total_stall_cy and total_msg_count across all output
      ports, compute avg_stall_cy = total_stall_cy / total_msg_count
    
    Stats in stats.txt:
      system.ruby.network.routersN.avg_stall_cy
      system.ruby.network.routersN.throttleNN.avg_stall_cy

--- src/mem/ruby/network/simple/Switch.hh
+++ src/mem/ruby/network/simple/Switch.hh
@@ -79,7 +79,6 @@ class SimpleNetwork;
 class Switch : public BasicRouter
 {
   public:
-
     // Makes sure throttle sends messages to the links after the switch is
     // done forwarding the messages in the same cycle
     static constexpr Event::Priority PERFECTSWITCH_EV_PRI = Event::Default_Pri;
@@ -90,34 +89,48 @@ class Switch : public BasicRouter
     ~Switch() = default;
     void init();
 
-    void addInPort(const std::vector<MessageBuffer*>& in);
+    void addInPort(const std::vector<MessageBuffer *> &in);
     void addOutPort(std::string switch_name,
-                    const std::vector<MessageBuffer*>& out,
-                    const NetDest& routing_table_entry,
-                    Cycles link_latency, int link_weight, int bw_multiplier,
-                    bool is_external,
+                    const std::vector<MessageBuffer *> &out,
+                    const NetDest &routing_table_entry, Cycles link_latency,
+                    int link_weight, int bw_multiplier, bool is_external,
                     PortDirection dst_inport = "");
 
-    const statistics::Formula & getMsgCount(unsigned int type) const
-    { return *(switchStats.m_msg_counts[type]); }
+    const statistics::Formula &
+    getMsgCount(unsigned int type) const
+    {
+        return *(switchStats.m_msg_counts[type]);
+    }
 
-    void print(std::ostream& out) const;
-    void init_net_ptr(SimpleNetwork* net_ptr) { m_network_ptr = net_ptr; }
-    SimpleNetwork* getNetPtr() const { return m_network_ptr; }
+    void print(std::ostream &out) const;
+    void
+    init_net_ptr(SimpleNetwork *net_ptr)
+    {
+        m_network_ptr = net_ptr;
+    }
+    SimpleNetwork *
+    getNetPtr() const
+    {
+        return m_network_ptr;
+    }
 
     bool functionalRead(Packet *);
-    bool functionalRead(Packet *, WriteMask&);
+    bool functionalRead(Packet *, WriteMask &);
     uint32_t functionalWrite(Packet *);
 
-    BaseRoutingUnit& getRoutingUnit() { return m_routing_unit; }
+    BaseRoutingUnit &
+    getRoutingUnit()
+    {
+        return m_routing_unit;
+    }
 
   private:
     // Private copy constructor and assignment operator
-    Switch(const Switch& obj);
-    Switch& operator=(const Switch& obj);
+    Switch(const Switch &obj);
+    Switch &operator=(const Switch &obj);
 
     PerfectSwitch perfectSwitch;
-    SimpleNetwork* m_network_ptr;
+    SimpleNetwork *m_network_ptr;
     std::list<Throttle> throttles;
 
     const Cycles m_int_routing_latency;
@@ -126,8 +139,7 @@ class Switch : public BasicRouter
     BaseRoutingUnit &m_routing_unit;
 
     unsigned m_num_connected_buffers;
-    std::vector<MessageBuffer*> m_port_buffers;
-
+    std::vector<MessageBuffer *> m_port_buffers;
 
   public:
     struct SwitchStats : public statistics::Group
@@ -141,13 +153,16 @@ class Switch : public BasicRouter
 
         // Statistical variables
         statistics::Formula percent_links_utilized;
+        statistics::Formula total_stall_cy;
+        statistics::Formula total_msg_count;
+        statistics::Formula avg_stall_cy;
         std::vector<statistics::Formula *> m_msg_counts;
         std::vector<statistics::Formula *> m_msg_bytes;
     } switchStats;
 };
 
-inline std::ostream&
-operator<<(std::ostream& out, const Switch& obj)
+inline std::ostream &
+operator<<(std::ostream &out, const Switch &obj)
 {
     obj.print(out);
     out << std::flush;
